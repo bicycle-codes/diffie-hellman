3bd457976f51bf5c3d1ec25f24070c7bac878617
nichoth <nichoth@nichoth.com>
2024-12-14 12:08:17 -0800

    setup

---
+++ src/dh.ts
@@ -0,0 +1,164 @@
+import MillerRabin from 'miller-rabin'
+import BN from './bn.js'
+import primes from './generatePrime.js'
+
+const millerRabin = new MillerRabin()
+const TWENTYFOUR = new BN(24)
+const ELEVEN = new BN(11)
+const TEN = new BN(10)
+const THREE = new BN(3)
+const SEVEN = new BN(7)
+module.exports = DH
+
+function setPublicKey (pub, enc) {
+    enc = enc || 'utf8'
+    if (!Buffer.isBuffer(pub)) {
+        pub = new Buffer(pub, enc)
+    }
+    this._pub = new BN(pub)
+    return this
+}
+
+function setPrivateKey (priv, enc) {
+    enc = enc || 'utf8'
+    if (!Buffer.isBuffer(priv)) {
+        priv = new Buffer(priv, enc)
+    }
+    this._priv = new BN(priv)
+    return this
+}
+
+const primeCache = {}
+function checkPrime (prime, generator) {
+    const gen = generator.toString('hex')
+    const hex = [gen, prime.toString(16)].join('_')
+    if (hex in primeCache) {
+        return primeCache[hex]
+    }
+    let error = 0
+
+    if (prime.isEven() ||
+    !primes.simpleSieve ||
+    !primes.fermatTest(prime) ||
+    !millerRabin.test(prime)) {
+    // not a prime so +1
+        error += 1
+
+        if (gen === '02' || gen === '05') {
+            // we'd be able to check the generator
+            // it would fail so +8
+            error += 8
+        } else {
+            // we wouldn't be able to test the generator
+            // so +4
+            error += 4
+        }
+        primeCache[hex] = error
+        return error
+    }
+    if (!millerRabin.test(prime.shrn(1))) {
+    // not a safe prime
+        error += 2
+    }
+    let rem
+    switch (gen) {
+        case '02':
+            if (prime.mod(TWENTYFOUR).cmp(ELEVEN)) {
+                // unsuidable generator
+                error += 8
+            }
+            break
+        case '05':
+            rem = prime.mod(TEN)
+            if (rem.cmp(THREE) && rem.cmp(SEVEN)) {
+                // prime mod 10 needs to equal 3 or 7
+                error += 8
+            }
+            break
+        default:
+            error += 4
+    }
+    primeCache[hex] = error
+    return error
+}
+
+function DH (prime, generator, malleable) {
+    this.setGenerator(generator)
+    this.__prime = new BN(prime)
+    this._prime = BN.mont(this.__prime)
+    this._primeLen = prime.length
+    this._pub = undefined
+    this._priv = undefined
+    this._primeCode = undefined
+    if (malleable) {
+        this.setPublicKey = setPublicKey
+        this.setPrivateKey = setPrivateKey
+    } else {
+        this._primeCode = 8
+    }
+}
+Object.defineProperty(DH.prototype, 'verifyError', {
+    enumerable: true,
+    get: function () {
+        if (typeof this._primeCode !== 'number') {
+            this._primeCode = checkPrime(this.__prime, this.__gen)
+        }
+        return this._primeCode
+    }
+})
+DH.prototype.generateKeys = function () {
+    if (!this._priv) {
+        this._priv = new BN(randomBytes(this._primeLen))
+    }
+    this._pub = this._gen.toRed(this._prime).redPow(this._priv).fromRed()
+    return this.getPublicKey()
+}
+
+DH.prototype.computeSecret = function (other) {
+    other = new BN(other)
+    other = other.toRed(this._prime)
+    const secret = other.redPow(this._priv).fromRed()
+    let out = new Buffer(secret.toArray())
+    const prime = this.getPrime()
+    if (out.length < prime.length) {
+        const front = new Buffer(prime.length - out.length)
+        front.fill(0)
+        out = Buffer.concat([front, out])
+    }
+    return out
+}
+
+DH.prototype.getPublicKey = function getPublicKey (enc) {
+    return formatReturnValue(this._pub, enc)
+}
+
+DH.prototype.getPrivateKey = function getPrivateKey (enc) {
+    return formatReturnValue(this._priv, enc)
+}
+
+DH.prototype.getPrime = function (enc) {
+    return formatReturnValue(this.__prime, enc)
+}
+
+DH.prototype.getGenerator = function (enc) {
+    return formatReturnValue(this._gen, enc)
+}
+
+DH.prototype.setGenerator = function (gen, enc) {
+    enc = enc || 'utf8'
+    if (!Buffer.isBuffer(gen)) {
+        gen = new Buffer(gen, enc)
+    }
+    this.__gen = gen
+    this._gen = new BN(gen)
+    return this
+}
+
+function formatReturnValue (bn, enc) {
+    const buf = new Buffer(bn.toArray())
+    if (!enc) {
+        return buf
+    } else {
+        return buf.toString(enc)
+    }
+}
---
+++ src/generatePrime.ts
@@ -0,0 +1,103 @@
+const randomBytes = require('randombytes')
+module.exports = findPrime
+findPrime.simpleSieve = simpleSieve
+findPrime.fermatTest = fermatTest
+const BN = require('bn.js')
+const TWENTYFOUR = new BN(24)
+const MillerRabin = require('miller-rabin')
+const millerRabin = new MillerRabin()
+const ONE = new BN(1)
+const TWO = new BN(2)
+const FIVE = new BN(5)
+const SIXTEEN = new BN(16)
+const EIGHT = new BN(8)
+const TEN = new BN(10)
+const THREE = new BN(3)
+const SEVEN = new BN(7)
+const ELEVEN = new BN(11)
+const FOUR = new BN(4)
+const TWELVE = new BN(12)
+let primes = null
+
+function _getPrimes () {
+    if (primes !== null) { return primes }
+
+    const limit = 0x100000
+    const res = []
+    res[0] = 2
+    for (let i = 1, k = 3; k < limit; k += 2) {
+        const sqrt = Math.ceil(Math.sqrt(k))
+        for (var j = 0; j < i && res[j] <= sqrt; j++) {
+            if (k % res[j] === 0) { break }
+        }
+
+        if (i !== j && res[j] <= sqrt) { continue }
+
+        res[i++] = k
+    }
+    primes = res
+    return res
+}
+
+function simpleSieve (p) {
+    const primes = _getPrimes()
+
+    for (let i = 0; i < primes.length; i++) {
+        if (p.modn(primes[i]) === 0) {
+            if (p.cmpn(primes[i]) === 0) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+function fermatTest (p) {
+    const red = BN.mont(p)
+    return TWO.toRed(red).redPow(p.subn(1)).fromRed().cmpn(1) === 0
+}
+
+function findPrime (bits, gen) {
+    if (bits < 16) {
+    // this is what openssl does
+        if (gen === 2 || gen === 5) {
+            return new BN([0x8c, 0x7b])
+        } else {
+            return new BN([0x8c, 0x27])
+        }
+    }
+    gen = new BN(gen)
+
+    let num, n2
+
+    while (true) {
+        num = new BN(randomBytes(Math.ceil(bits / 8)))
+        while (num.bitLength() > bits) {
+            num.ishrn(1)
+        }
+        if (num.isEven()) {
+            num.iadd(ONE)
+        }
+        if (!num.testn(1)) {
+            num.iadd(TWO)
+        }
+        if (!gen.cmp(TWO)) {
+            while (num.mod(TWENTYFOUR).cmp(ELEVEN)) {
+                num.iadd(FOUR)
+            }
+        } else if (!gen.cmp(FIVE)) {
+            while (num.mod(TEN).cmp(THREE)) {
+                num.iadd(FOUR)
+            }
+        }
+        n2 = num.shrn(1)
+        if (simpleSieve(n2) && simpleSieve(num) &&
+      fermatTest(n2) && fermatTest(num) &&
+      millerRabin.test(n2) && millerRabin.test(num)) {
+            return num
+        }
+    }
+}
